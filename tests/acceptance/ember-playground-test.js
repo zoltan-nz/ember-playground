9fa7c8e8373db4bbc2994d210d6542cd24e811b8
Zoltan <zdebre@gmail.com>
2021-02-15 12:52:00 +1300

    Add page link acceptance test

---
+++ tests/acceptance/ember-playground-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { click, visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | ember playground', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.strictEqual(currentURL(), '/');
+    assert.dom('h2').hasText('Welcome to Super Rentals!');
+
+    assert.dom('.jumbo a.button').hasText('About Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
+});
