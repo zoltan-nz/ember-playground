2e8a1aede493d39dd94a2140e3e82d618ac26578
Zoltan <zdebre@gmail.com>
2021-02-16 12:57:00 +1300

    Add more acceptance test

--- tests/acceptance/ember-playground-test.js
+++ tests/acceptance/ember-playground-test.js
@@ -16,4 +16,28 @@ module('Acceptance | ember playground', function (hooks) {
 
     assert.strictEqual(currentURL(), '/about');
   });
+
+  test('visiting /about', async function (assert) {
+    await visit('/about');
+
+    assert.strictEqual(currentURL(), '/about');
+    assert.dom('h2').hasText('About Super Rentals');
+
+    assert.dom('.jumbo a.button').hasText('Contact Us');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+  });
+
+  test('visiting /getting-in-touch', async function (assert) {
+    await visit('/getting-in-touch');
+
+    assert.strictEqual(currentURL(), '/getting-in-touch');
+    assert.dom('h2').hasText('Contact Us');
+
+    assert.dom('.jumbo a.button').hasText('About');
+    await click('.jumbo a.button');
+
+    assert.strictEqual(currentURL(), '/about');
+  });
 });
