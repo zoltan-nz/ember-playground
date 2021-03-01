d5826e6118363156a31f4f9b1d728caea65ba124
Zoltan <zdebre@gmail.com>
2021-03-01 15:22:00 +1300

    Add Rental::Image namespaced component test

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,26 +1,23 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
 
 module('Integration | Component | rental/image', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Rental::Image />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
+  test('it renders the given image', async function (assert) {
     await render(hbs`
-      <Rental::Image>
-        template block text
-      </Rental::Image>
+        <Rental::Image
+         src='/assets/images/teaching-tomster.png'
+         alt='Teaching Tomster'
+         />
     `);
 
-    assert.dom(this.element).hasText('template block text');
+    assert
+      .dom('.image img')
+      .exists()
+      .hasAttribute('src', '/assets/images/teaching-tomster.png')
+      .hasAttribute('alt', 'Teaching Tomster');
   });
 });
