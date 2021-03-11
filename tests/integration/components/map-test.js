741f4e7d2db12d04460ffa3c3de8769bf1ec0b3a
Zoltan <zdebre@gmail.com>
2021-03-11 18:16:00 +1300

    Add map component test

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -1,26 +1,35 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import ENV from 'ember-playground/config/environment';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+ENV.MAPBOX_ACCESS_TOKEN = 'pk.test';
 
 module('Integration | Component | map', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Map />`);
-
-    assert.dom(this.element).hasText('');
-
-    // Template block usage:
+  test('it renders a map image for the specified parameters', async function (assert) {
     await render(hbs`
-      <Map>
-        template block text
-      </Map>
+      <Map
+        @lat="37.7797"
+        @lng="-122.4184"
+        @zoom="10"
+        @width="150"
+        @height="120"
+      />
     `);
 
-    assert.dom(this.element).hasText('template block text');
+    assert
+      .dom('.map img')
+      .exists()
+      .hasAttribute('alt', 'Map image at coordinates 37.7797,-122.4184')
+      .hasAttribute('src')
+      .hasAttribute('width', '150')
+      .hasAttribute('height', '120');
+
+    let { src } = find('.map img');
+
+    assert.ok(src.includes(ENV.MAPBOX_ACCESS_TOKEN));
   });
 });
