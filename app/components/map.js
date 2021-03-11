809d680a0b56cef3967afaf8a00d01bfd01ffdaf
Zoltan <zdebre@gmail.com>
2021-03-12 00:21:00 +1300

    Move logic from map template to js file

--- app/components/map.js
+++ app/components/map.js
@@ -1,7 +1,19 @@
 import Component from '@glimmer/component';
 import ENV from 'ember-playground/config/environment';
 
+const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
+
 export default class MapComponent extends Component {
+  get src() {
+    const { lng, lat, width, height, zoom } = this.args;
+
+    const coordinates = `${lng},${lat},${zoom}`;
+    const dimensions = `${width}x${height}`;
+    const accessToken = `access_token=${this.token}`;
+
+    return `${MAPBOX_API}/${coordinates}/${dimensions}@2x?${accessToken}`;
+  }
+
   get token() {
     return encodeURIComponent(ENV.MAPBOX_ACCESS_TOKEN);
   }
