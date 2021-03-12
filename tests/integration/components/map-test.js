c15f155f3c3a4d02d10ab661233c3047eef30d38
Zoltan <zdebre@gmail.com>
2021-03-13 00:29:00 +1300

    Extend map component test with property test

--- tests/integration/components/map-test.js
+++ tests/integration/components/map-test.js
@@ -32,4 +32,28 @@ module('Integration | Component | map', function (hooks) {
 
     assert.ok(src.includes(ENV.MAPBOX_ACCESS_TOKEN));
   });
+
+  test('src attributes', async function (assert) {
+    this.setProperties({
+      lat: 37.7797,
+      lng: -122.4184,
+      zoom: 10,
+      width: 150,
+      height: 120,
+    });
+
+    await render(hbs`
+      <Map
+        @lat={{this.lat}}
+        @lng={{this.lng}}
+        @zoom={{this.zoom}}
+        @width={{this.width}}
+        @height={{this.height}}
+      />
+    `);
+
+    const img = find('.map img');
+
+    assert.ok(img.src.includes('-122.4184,37.7797,10'));
+  });
 });
