cb03d4f790f9a6c6d63a05f3d3ccd22a0c19ecd7
Zoltan <zdebre@gmail.com>
2021-03-14 00:43:00 +1300

    Use route model

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+  async model() {
+    return {
+      title: 'Grand Old Mansion',
+      owner: 'Veruca Salt',
+      city: 'San Francisco',
+      location: {
+        lat: 37.7749,
+        lng: -122.4194,
+      },
+      category: 'Estate',
+      type: 'Standalone',
+      bedrooms: 15,
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      description:
+        'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.',
+    };
+  }
+}
