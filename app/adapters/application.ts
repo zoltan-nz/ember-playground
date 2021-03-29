9cd4ecff7fd0c24e3cd4a254e3a1750641df2e03
Zoltan <zdebre@gmail.com>
2021-03-30 00:47:00 +1300

    Convert ApplicationAdapter to TypeScript

--- app/adapters/application.ts
+++ app/adapters/application.ts
@@ -1,9 +1,10 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import DS from 'ember-data';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   namespace = 'api';
 
-  buildURL(...args) {
-    return `${super.buildURL(...args)}.json`;
+  buildURL(modelName?: string | number | undefined, id?: string | {} | any[] | null | undefined, snapshot?: any[] | DS.Snapshot<string | number> | null | undefined, requestType?: string | undefined, query?: {} | undefined): string {
+    return `${super.buildURL(modelName, id, snapshot, requestType, query)}.json`;
   }
 }
