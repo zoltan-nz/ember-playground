d36699f7404d722053aeeed83f3c5072e690072b
Zoltan <zdebre@gmail.com>
2021-04-01 01:02:00 +1300

    Convert Rental model to TypeScript

--- app/models/rental.ts
+++ app/models/rental.ts
@@ -2,15 +2,28 @@ import Model, { attr } from '@ember-data/model';
 
 const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
-export default class RentalModel extends Model {
-  @attr() title;
-  @attr() owner;
-  @attr() city;
-  @attr() location;
-  @attr() category;
-  @attr() image;
-  @attr() bedrooms;
-  @attr() description;
+export default class Rental extends Model {
+  @attr('string')
+  declare title: string;
+  @attr()
+  declare owner: string;
+  @attr()
+  declare city: string;
+
+  @attr()
+  declare location: string;
+
+  @attr()
+  declare category: string;
+
+  @attr()
+  declare image: string
+
+  @attr()
+  declare bedrooms: string;
+
+  @attr()
+  declare description: string;
 
   get type() {
     if (COMMUNITY_CATEGORIES.includes(this.category)) {
